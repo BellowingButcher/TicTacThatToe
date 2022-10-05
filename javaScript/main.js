e6d3f30af401bb28b46118d178bc3f86db670877
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-05 19:51:50 +0000

    built the changeGameBoardFuntion

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,15 @@
+let player = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
 function changePlayer() {
     if (player === 'X') {
         player = 'O';
     }    
     else {player = 'X'};
 }
+// function changeBoardState()
+//     - insert player into array
+//         - at selected players tile(index)
+function changeBoardState(tile){
+    gameBoard.splice(tile, 1, player);
+}
+
